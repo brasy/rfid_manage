20430db71c54f7b687815040d421954dadd692be
ying xin <395488553@qq.com>
2017-12-01 16:13:55 +0800

    Update check.js

--- bin/check.js
+++ bin/check.js
@@ -1,3 +1,27 @@
 /** * Created on 2017/9/11. */
-//崩溃自动重启程序var exec = require("child_process").exec;check();function check() {    last = exec("lsof -i:3020");    last.on("exit",function (code) {        if(code !='0'){            console.log('主服务已经关闭,正在重启....');            run();        }else {            console.log('主服务器正常运行中');        }    });    setTimeout(check,5000);}
-function run() {    last = exec("node www");    last.on('exit',function (code) {        if(code == "0"){            console.log("主服务已经启动成功");        }else {            console.log("主服务启动失败");        }    })}
+//崩溃自动重启程序
+var exec = require("child_process").exec;
+check();
+function check() {
+  last = exec("lsof -i:3020");
+  last.on("exit",function (code) {
+    if(code !='0'){
+      console.log('主服务已经关闭,正在重启....');
+      run();
+    }else {
+      console.log('主服务器正常运行中');
+    }
+  });
+  setTimeout(check,5000);
+}
+
+function run() {
+  last = exec("node www");
+  last.on('exit',function (code) {
+    if(code == "0"){
+      console.log("主服务已经启动成功");
+    }else {
+      console.log("主服务启动失败");
+    }
+  })
+}
