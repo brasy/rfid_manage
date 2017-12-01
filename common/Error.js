9e3de8b92f5f579c876ba3136a3846d6846ed711
ying xin <395488553@qq.com>
2017-12-01 16:10:24 +0800

    Create Error.js

---
+++ common/Error.js
@@ -0,0 +1,5 @@
+/*  #!/usr/local/bin/node  -*- coding:utf-8 -*-  Desc: Error definition and inherit */
+var config = require("../config/cfg.js");var assert = require("assert");var util   = require("util");
+function DBError (message) {    Error.call(this);    this.name       = "DBError";    this.message    = message || "DBError";    this.statusCode = config.statusCode.STATUS_DBERROR;}
+util.inherits(DBError, Error);
+global.DBError           = DBError;
